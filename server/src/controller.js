852c74321a8451245d8e87bdff8f3b41afe48a51
Alberto-007 <asj3@aluno.ifal.edu.br>
2023-12-26 15:57:26 -0300

    new file:   server/src/controller.js 	modified:   server/src/routes.js

---
+++ server/src/controller.js
@@ -0,0 +1,45 @@
+import pool from "./database.js";
+
+//create
+export const createTodo = async (req, res) => {
+    const id = req.body["id"];
+    const title = req.body["title"];
+  
+    console.log(id);
+    console.log(title);
+  
+    pool.query('INSERT INTO todolist (id, title) VALUES ($1, $2)', [id, title])
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+  
+    res.status(201).send('Todo criado com sucesso');
+  };
+  
+ //read 
+  export const readTodos = async (req, res) => {
+    pool.query('SELECT * FROM todolist')
+      .then(result => res.json(result.rows))
+      .catch(err => console.log(err));
+  };
+  
+  //read pelo id
+  export const readTodoById = async (req, res) => {
+    pool.query('SELECT * FROM todolist WHERE id = $1', [req.params.id])
+      .then(result => res.json(result.rows))
+      .catch(err => console.log(err));
+  };
+  
+  //update
+  export const updateTodo = async (req, res) => {
+    pool.query('UPDATE todolist SET title = $1 WHERE id = $2', [req.body.title, req.params.id])
+      .then(response => res.status(200).send('Todo atualizado com sucesso'))
+      .catch(err => console.log(err));
+  };
+  
+  //delete
+  export const deleteTodo = async (req, res) => {
+    pool.query('DELETE FROM todolist WHERE id = $1', [req.params.id])
+      .then(response => res.status(200).send('Todo deletado com sucesso'))
+      .catch(err => console.log(err));
+  };
+  
--- server/src/routes.js
+++ server/src/routes.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import pool from './database.js'
+import { createTodo, readTodos, readTodoById, updateTodo, deleteTodo } from './controller.js'
 
 const router = express.Router()
 
@@ -9,48 +10,10 @@ router.get('/', (req, res) => {
 })
 
 //Rotas
-
-//create
-router.post('/todos', (req, res) => {
-    const id = req.body["id"]
-    const title = req.body["title"]
-
-    console.log(id)
-    console.log(title)
-
-    pool.query('INSERT INTO todolist (id, title) VALUES ($1, $2)', [id, title])
-    .then(res => console.log(res))
-    .catch(err => console.log(err))
-
-    res.status(201).send('Todo criado com sucesso')
-})
-
-//read
-router.get(('/todos'), (req, res) => {
-    pool.query('SELECT * FROM todolist')
-    .then(result => res.json(result.rows))
-    .catch(err => console.log(err))
-})
-
-//read pelo id
-router.get(('/todos/:id'), (req, res) => {
-    pool.query('SELECT * FROM todolist WHERE id = $1', [req.params.id])
-    .then(result => res.json(result.rows))
-    .catch(err => console.log(err))
-})
-
-//update
-router.put('/todos/:id', (req, res) => {
-    pool.query('UPDATE todolist SET title = $1 WHERE id = $2', [req.body.title, req.params.id])
-    .then(response => res.status(200).send('Todo atualizado com sucesso'))
-    .catch(err => console.log(err))
-})
-
-//delete
-router.delete('/todos/:id', (req, res) => {
-    pool.query('DELETE FROM todolist WHERE id = $1', [req.params.id])
-    .then(response => res.status(200).send('Todo deletado com sucesso'))
-    .catch(err => console.log(err))
-})
+router.get('/todos', readTodos)
+router.get('/todos/:id', readTodoById)
+router.post('/todos', createTodo)
+router.put('/todos/:id', updateTodo)
+router.delete('/todos/:id', deleteTodo)
 
 export default router
